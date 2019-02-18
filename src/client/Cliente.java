75e378c3bcc392d70f14fe9b5d84c97f35625435
Vicente Silva <vicente.pspenha@gmail.com>
2019-02-18 16:56:22 -0300

    modificações/atualizações

--- src/client/Cliente.java
+++ src/client/Cliente.java
@@ -4,54 +4,53 @@ import util.Mensagem;
 import util.Status;
 
 import javax.swing.*;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Cliente {
 
     private Socket socket;
 
     public static void main(String[] args) {
-//        String ip = JOptionPane.showInputDialog(null, "Informe o ip do servidor");
-        String ip = "localhost";
-        do{
-//            String porta = JOptionPane.showInputDialog(null, "Informe a porta do servidor");
-            String porta = "8000";
-            try {
-                if (Integer.parseInt(porta) > 0 && Integer.parseInt(porta) < 80000){
-                    new Cliente(ip, Integer.parseInt(porta));
-                    break;
-                }
-            }catch (Exception e){
-                JOptionPane.showMessageDialog(null, "Informe uma porta válida. Error: " + e.getMessage());
-            }
-        }while (true);
+        new Cliente("localhost", 3322);
     }
 
-    public Cliente(String ip, int porta) {
+    public Cliente(String local, int porta) {
         try {
-            System.out.println("Estabelendo conexão com servidor...");
-            socket = new Socket(ip, porta);
-            System.out.println("Conexão com servidor estabelecida.");
+            System.out.println("Estabelecendo conexão...");
+            socket = new Socket(local, porta);
+            System.out.println("Conexão estabelecida.");
 
-            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream objInput = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream objOutput = new ObjectOutputStream(socket.getOutputStream());
 
-            System.out.println("Enviando mensagem para o servidor.");
+            System.out.println("Enviando mensagem...");
 
-            Mensagem mensagem = new Mensagem("teste");
+            Mensagem mensagem = new Mensagem("CLIENT");
             mensagem.setStatus(Status.SOLICITACAO);
             mensagem.setParam("nome", "Eduardo");
             mensagem.setParam("sobrenome", "Dipp");
+            objOutput.writeObject(mensagem);
+            objOutput.flush();
 
-            objectOutputStream.writeObject(mensagem);
-            objectOutputStream.flush();
-        } catch (IOException e){
-            JOptionPane.showMessageDialog(null, "Erro de conexão no cliente. Error: " + e.getMessage());
+            System.out.println("\nMensagem\n" + mensagem + " enviando.\n");
+
+            mensagem = (Mensagem) objInput.readObject();
+
+            if(mensagem.getStatus() == Status.OK){
+                System.out.println("Resposta\n" + mensagem);
+//                String resp = (String) mensagem.getParam("mensagem");
+//                System.out.println("Mensagem: " + resp + "\n");
+            }else{
+                System.out.println("Erro: " + mensagem.getStatus() + "\n");
+            }
+
+            objInput.close();
+            objOutput.close();
+            socket.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro no Cliente: " + e.getMessage());
         }
     }
 }
